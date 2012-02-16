8e83e5d94f08a820df546153403d55f7487c3161
Dominic LoBue <dominic.lobue@gmail.com>
2012-02-15 23:59:20 -0800

    update stateless untar_from_s3 provider to use fog instead of telegraph

--- cookbooks/solo_client/providers/untar_from_s3v2.rb
+++ cookbooks/solo_client/providers/untar_from_s3v2.rb
@@ -1,10 +1,11 @@
 
 require 'fileutils'
+require 'md5'
 
 action :run do
 
     if new_resource.user.nil?
-        user = node[:env][:geo_user]
+        user = node[:env][:user]
     else
         user = new_resource.user
     end
@@ -12,6 +13,20 @@ action :run do
 
     Chef::Log.debug("untar_from_s3: artifact #{new_resource.artifact} to path #{new_resource.container_path}")
     Chef::Log.debug("untar_from_s3: user: #{user}")
+
+    storage = Fog::Storage.new(get_creds().merge(:provider => 'AWS'))
+    bucket = storage.directories.get(node.env.s3_bucket)
+
+    artifacts = bucket.files.all(
+        :prefix => [node.env.s3_folder, new_resource.artifact].join('/')
+    ).to_a.select{ |k|
+        not k.key.split('/')[-1].match(/.+-[^-]+\.t(ar\.)?(gz|bz2)/).nil?
+    }
+
+    raise "no artifacts found!" if artifacts.empty?
+
+    artifact = artifacts[0]
+    fileName = artifact.key.split('/')[-1]
 
     # First, see if there is a new archive to download.
     if node.chef_packages.chef.version.split('.')[1].to_i < 10
@@ -30,22 +45,31 @@ action :run do
     end
 
 
-    archivePath = node[:env][:archive_dir_Pathname] + fileName
+    archivePath = node[:env][:archive_dir] + fileName
     createsPath = new_resource.creates.start_with?('/') ? new_resource.creates : ::File.join(new_resource.container_path, new_resource.creates)
 
+    #TODO: cleanup old and failed versions.
     #download block
     if ::File.exists?(archivePath)
+        md5sum = MD5.hexdigest(::File.read(archivePath))
+        raise "version on filesystem doesn't match the one on s3!" unless md5sum == artifact.etag
+        #TODO: delete bad file
+        #TODO: do something smarter that just raise
         Chef::Log.debug("latest version of artifact #{new_resource.artifact} already on disk. Skipping download and going straight to extraction.")
     else
         Chef::Log.info("downloading new archive to #{archivePath}")
         begin
-            Chef::Mixin::Command.run_command( :command => "telegraph download_file #{node[:env][:s3_bucket]} #{node[:env][:s3_folder]}/#{fileName} #{archivePath}")
+            File.open(archivePath, 'w') {|local_file|
+                local_file.write(artifact.body)} 
         rescue Chef::Exceptions::Exec => e
             ::File.delete( archivePath )
             raise e
         end
+        FileUtils.touch archivePath
         FileUtils.chown(user, user, archivePath)
     end
+
+    #TODO: check md5 of file we just downloaded to be sure it matches etag
 
     #extract block
     if not ::File.exists?(createsPath) or ::File.mtime(archivePath) > ::File.mtime(createsPath)
@@ -56,6 +80,7 @@ action :run do
         end
         Chef::Log.info("untaring archive #{archivePath} to #{new_resource.container_path}")
         begin
+            #TODO: support bzip2 compressed files
             Chef::Mixin::Command.run_command( :command => "tar -o -xzf #{archivePath} -C #{new_resource.container_path}", :user => user, :group => user)
         rescue Chef::Exceptions::Exec => e
             ::File.rename( archivePath, "#{archivePath}.failed" )
