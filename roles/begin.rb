429cd075471cbce2a3aac5c1fe990ae300686466
Dominic LoBue <dominic.lobue@gmail.com>
2012-02-16 15:20:58 -0800

    all apt recipes were refactored into their own cookbook - update begin role to point to new locaations.

--- roles/begin.rb
+++ roles/begin.rb
@@ -3,10 +3,10 @@ name "begin"
 description "recipes that should always run first."
 
 run_list(
-    "recipe[misc::apt_fix]",
+    "recipe[apt::apt_fix]",
+    "recipe[apt::default]",
     "recipe[misc::mounts]",
     "recipe[solo_client::auto_restart_chef]",
-    "recipe[solo_client::apt]",
     "recipe[misc::default]",
     "recipe[solo_client::begin_end_hub]"
 )
