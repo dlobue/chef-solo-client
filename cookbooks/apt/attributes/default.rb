fe7107dc57513615805426a1fbd5be848d818108
Dominic LoBue <dominic.lobue@gmail.com>
2012-02-21 16:56:44 -0800

    only put aws credentials in the aptrepo.list file if they are present.

--- cookbooks/apt/attributes/default.rb
+++ cookbooks/apt/attributes/default.rb
@@ -4,6 +4,9 @@ default.apt.bucket = nil
 default.apt.distro = os == "darwin" ? "darwin" : lsb.codename
 default.apt.components = %w{main}
 
+default.apt.aws_access_key_id = nil
+default.apt.aws_secret_access_key = nil
+
 default.apt.install_recommends = false
 default.apt.install_suggests = false
 
