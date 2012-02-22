39cdd42f79b6240849f3d40a135856d19a976f33
Dominic LoBue <dominic.lobue@gmail.com>
2012-02-21 17:10:01 -0800

    replace the old stateful untar_from_s3 resource with stateless version

--- cookbooks/solo_client/providers/untar_from_s3.rb
+++ cookbooks/solo_client/providers/untar_from_s3.rb
@@ -1,8 +1,11 @@
 
+require 'fileutils'
+require 'md5'
+
 action :run do
 
     if new_resource.user.nil?
-        user = node[:env][:geo_user]
+        user = node[:env][:user]
     else
         user = new_resource.user
     end
@@ -11,66 +14,63 @@ action :run do
     Chef::Log.debug("untar_from_s3: artifact #{new_resource.artifact} to path #{new_resource.container_path}")
     Chef::Log.debug("untar_from_s3: user: #{user}")
 
+    storage = Fog::Storage.new(get_creds().merge(:provider => 'AWS'))
+    bucket = storage.directories.get(node.env.s3_bucket)
+
     # First, see if there is a new archive to download.
-    if node.chef_packages.chef.version.split('.')[1].to_i < 10
-        cmd = "telegraph list_artifact #{node[:env][:s3_bucket]} #{node[:env][:s3_folder]} #{new_resource.artifact}"
-        Chef::Log.debug("cmd: #{cmd}")
-        output = `#{cmd}`
-        if $?.to_i != 0
-            raise "telegraph error:\n#{output}"
-        end
-        fileName = output.strip.split("\t")[0]
+    artifacts = bucket.files.all(
+        :prefix => [node.env.s3_folder, new_resource.artifact].join('/')
+    ).to_a.select{ |k|
+        not k.key.split('/')[-1].match(/.+-[^-]+\.t(ar\.)?(gz|bz2)/).nil?
+    }
+
+    raise "no artifacts found!" if artifacts.empty?
+
+    artifact = artifacts[0]
+    fileName = artifact.key.split('/')[-1]
+
+    archivePath = node[:env][:archive_dir] + fileName
+    createsPath = new_resource.creates.start_with?('/') ? new_resource.creates : ::File.join(new_resource.container_path, new_resource.creates)
+
+    #TODO: cleanup old and failed versions.
+    #download block
+    if ::File.exists?(archivePath)
+        md5sum = MD5.hexdigest(::File.read(archivePath))
+        raise "version on filesystem doesn't match the one on s3!" unless md5sum == artifact.etag
+        #TODO: delete bad file
+        #TODO: do something smarter that just raise
+        Chef::Log.debug("latest version of artifact #{new_resource.artifact} already on disk. Skipping download and going straight to extraction.")
     else
-        cmd = Chef::ShellOut.new("telegraph list_artifact #{node[:env][:s3_bucket]} #{node[:env][:s3_folder]} #{new_resource.artifact}")
-        cmd.run_command
-        cmd.error!
-        fileName = cmd.stdout.strip.split("\t")[0]
+        Chef::Log.info("downloading new archive to #{archivePath}")
+        begin
+            File.open(archivePath, 'w') {|local_file|
+                local_file.write(artifact.body)} 
+        rescue Chef::Exceptions::Exec => e
+            ::File.delete( archivePath )
+            raise e
+        end
+        FileUtils.touch archivePath
+        FileUtils.chown(user, user, archivePath)
     end
 
+    #TODO: check md5 of file we just downloaded to be sure it matches etag
 
-    if node[:persist]["artifact_#{new_resource.artifact}".to_sym] == fileName
-        Chef::Log.debug("Artifact #{new_resource.artifact} already in place.")
-        new_resource.updated_by_last_action(false)
-    else
-
-        archivePath = node[:env][:archive_dir_Pathname] + fileName
-        if ::File.exists?(archivePath)
-            Chef::Log.debug("latest version of artifact #{new_resource.artifact} already on disk. Skipping download and going straight to extraction.")
-        else
-            if node[:persist]["artifact_#{new_resource.artifact}".to_sym]
-                Chef::Log.info("getting rid of old version of artifact: rm -rf #{node[:env][:archive_dir_Pathname] + node[:persist]["artifact_#{new_resource.artifact}".to_sym]}")
-                Chef::Mixin::Command.run_command(
-                    :command => "rm -rf #{node[:env][:archive_dir_Pathname] + node[:persist]["artifact_#{new_resource.artifact}".to_sym]}" # scary
-                )
-            end
-            Chef::Log.info("downloading new archive to #{archivePath}")
-            Chef::Mixin::Command.run_command(
-                :command => "telegraph download_file #{node[:env][:s3_bucket]} #{node[:env][:s3_folder]}/#{fileName} #{archivePath}"
-            )
-            Chef::Mixin::Command.run_command(
-                :command => "chown #{user}:#{user} #{archivePath}"
-            )
-        end
+    #extract block
+    if not ::File.exists?(createsPath) or ::File.mtime(archivePath) > ::File.mtime(createsPath)
+        #delete block
         if not new_resource.delete_dir_in_container.nil?
-            Chef::Log.info("deleting dir_in_container: rm -rf #{new_resource.container_path}/#{new_resource.delete_dir_in_container}")
-            Chef::Mixin::Command.run_command(
-                :command => "rm -rf #{new_resource.container_path}/#{new_resource.delete_dir_in_container}" # scary
-            )
+            Chef::Log.info("deleting dir_in_container: #{new_resource.container_path}/#{new_resource.delete_dir_in_container}")
+            FileUtils.remove_dir( "#{new_resource.container_path}/#{new_resource.delete_dir_in_container}", true )
         end
         Chef::Log.info("untaring archive #{archivePath} to #{new_resource.container_path}")
         begin
-            Chef::Mixin::Command.run_command(
-                :command => "tar -o -xzf #{archivePath} -C #{new_resource.container_path}",
-                :user => user,
-                :group => user
-            )
+            #TODO: support bzip2 compressed files
+            Chef::Mixin::Command.run_command( :command => "tar -o -xzf #{archivePath} -C #{new_resource.container_path}", :user => user, :group => user)
         rescue Chef::Exceptions::Exec => e
-            Chef::Mixin::Command.run_command(
-                :command => "mv #{archivePath} #{archivePath}.failed"
-            )
-            raise ArtifactExtractionError, "untarring #{archivePath} failed for some reason."
+            ::File.rename( archivePath, "#{archivePath}.failed" )
+            raise e
         end
-        node[:persist]["artifact_#{new_resource.artifact}".to_sym] = fileName
+        FileUtils.touch createsPath
         new_resource.updated_by_last_action(true)
     end
 end
--- cookbooks/solo_client/resources/untar_from_s3.rb
+++ cookbooks/solo_client/resources/untar_from_s3.rb
@@ -6,6 +6,7 @@ attribute :artifact, :kind_of => String, :name_attribute => true
 attribute :container_path, :kind_of => [String, Pathname], :required => true
 attribute :delete_dir_in_container, :kind_of => [String, Pathname]
 attribute :user, :kind_of => String
+attribute :creates, :kind_of => [String, Pathname], :required => true
 
 def initialize(*args)
     super
