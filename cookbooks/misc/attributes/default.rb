cbb57da87a1648f141f6f82289bf0804fd06dd4e
Dominic LoBue <dominic.lobue@gmail.com>
2012-03-02 16:34:11 -0800

    update the misc.uses attribute to be updatable (instead of just overridable)

--- cookbooks/misc/attributes/default.rb
+++ cookbooks/misc/attributes/default.rb
@@ -5,11 +5,13 @@ default.misc.distutils_conf_file = '/root/.pydistutils.cfg'
 default.ver_minor = 'none'
 default.mounts = {}
 
-default.misc.users = ['root'] + [
-    'ubuntu',
-].uniq.select do |user|
-    etc.passwd.has_key?(user) and not ['nologin', 'false'].include?(
-        etc.passwd[user.to_sym].shell.split('/')[-1]
-    )
+default.misc.extrausers = []
+default.misc.users = Promise.new do ['root'] + [misc.extrausers].flatten + [
+        'ubuntu',
+    ].uniq.select do |user|
+        etc.passwd.has_key?(user) and not ['nologin', 'false'].include?(
+            etc.passwd[user.to_sym].shell.split('/')[-1]
+        )
+    end
 end
 
