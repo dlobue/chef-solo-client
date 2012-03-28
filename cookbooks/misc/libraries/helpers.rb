fcd28ba315001b1d2bb42f96de266948b94ab56e
Dominic LoBue <dominic.lobue@gmail.com>
2012-03-28 16:32:35 -0700

    add helper function to convert strings like '6473kB' into integers, and then into normalize into bytes

--- cookbooks/misc/libraries/helpers.rb
+++ cookbooks/misc/libraries/helpers.rb
@@ -12,3 +12,26 @@ class Promise < Proc
     end
 end
 
+
+def str_to_bytes(s)
+    r = s.match(/^[0-9]+/)
+    amount = r.to_s.to_i
+    units = r.post_match
+
+    units.each_char { |c|
+        case c
+        when "k", "K"
+            amount *= (1024**1)
+        when "m", "M"
+            amount *= (1024**2)
+        when "g", "G"
+            amount *= (1024**3)
+        when "b"
+            amount /= 8
+        when "B"
+            amount /= 1
+        end
+    }
+    return amount
+end
+
