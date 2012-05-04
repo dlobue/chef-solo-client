c86bd0445e284f67ded69faaa29518276ba91b89
Leo Deza <ldeza@beachmint.com>
2012-05-04 14:57:12 -0700

    cruft removal

--- cookbooks/solo_client/recipes/creds.rb
+++ cookbooks/solo_client/recipes/creds.rb
@@ -1,14 +1,6 @@
 
 cookbook_dir = Array(Chef::Config[:cookbook_path]).detect{|e| e =~ /\/cookbooks\/*$/ }
 
-#solo_client_untar_from_s3 "creds" do
-#    only_if { node.attribute?("get_creds") and node.get_creds }
-#    user "root"
-#    container_path cookbook_dir
-#    creates File.join(cookbook_dir, 'cred_cookbook')
-#    notifies :create, "ruby_block[apply_creds]", :immediately
-#end
-
 s3_artifact "creds" do
   only_if { node.attribute?("get_creds") and node.get_creds }
   action [:create, :untar]
