368256caf139505db285c2169bd7945725e6e317
Leo Deza <ldeza@beachmint.com>
2012-05-04 14:39:47 -0700

    replaced depreciated untar_from_s3 with new hotness s3_artifact

--- cookbooks/solo_client/recipes/creds.rb
+++ cookbooks/solo_client/recipes/creds.rb
@@ -1,12 +1,27 @@
 
 cookbook_dir = Array(Chef::Config[:cookbook_path]).detect{|e| e =~ /\/cookbooks\/*$/ }
 
-solo_client_untar_from_s3 "creds" do
-    only_if { node.attribute?("get_creds") and node.get_creds }
-    user "root"
-    container_path cookbook_dir
-    creates File.join(cookbook_dir, 'cred_cookbook')
-    notifies :create, "ruby_block[apply_creds]", :immediately
+#solo_client_untar_from_s3 "creds" do
+#    only_if { node.attribute?("get_creds") and node.get_creds }
+#    user "root"
+#    container_path cookbook_dir
+#    creates File.join(cookbook_dir, 'cred_cookbook')
+#    notifies :create, "ruby_block[apply_creds]", :immediately
+#end
+
+s3_artifact "creds" do
+  only_if { node.attribute?("get_creds") and node.get_creds }
+  action [:create, :untar]
+  user "root"
+  bucket node.env.s3_bucket
+  folder node.env.s3_folder
+  artifact name
+  path node.env.archive_dir + "#{name}.tgz"
+
+  container_path cookbook_dir
+  creates File.join(cookbook_dir, 'cred_cookbook')
+
+  notifies :create, "ruby_block[apply_creds]", :immediately
 end
 
 ruby_block "apply_creds" do
