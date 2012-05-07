2d6d255c7db96373ca75045477c969cdf2ed8990
Dominic LoBue <dominic.lobue@gmail.com>
2012-05-07 16:27:00 -0700

    some package names in ubuntu precise have changed.

--- cookbooks/misc/recipes/base_pkgs.rb
+++ cookbooks/misc/recipes/base_pkgs.rb
@@ -1,4 +1,8 @@
 
+node.lsb.codename
+suite_pkg_names = {"precise" => { "ctags" => "exuberant-ctags" }}
+pkg_names_override = suite_pkg_names[node.lsb.codename] || {}
+
 %w{
   git-completion
   git-core
@@ -11,6 +15,8 @@
   vim-nox
   screen
 }.each do |pkg|
+  pkg_override = pkg_names_override[pkg]
+  pkg = pkg_override unless pkg_override.nil?
   package pkg do
     action :upgrade
   end
