d910de034acda2f5d7da5a30e5f9045c10ee6c88
Dominic LoBue <dominic.lobue@gmail.com>
2012-05-22 17:31:22 -0700

    add support for specifying architectures to use an apt repo for.

--- cookbooks/apt/attributes/default.rb
+++ cookbooks/apt/attributes/default.rb
@@ -3,6 +3,7 @@ default.apt.repo_key = nil
 default.apt.bucket = nil
 default.apt.distro = os == "darwin" ? "darwin" : lsb.codename
 default.apt.components = %w{main}
+default.apt.arches = %w{all amd64}
 default.apt.config_dir = Pathname.new "/etc/apt"
 default.apt.sources_dir = Promise.new { apt.config_dir + 'sources.list.d' }
 
