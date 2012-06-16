845e05f41035ea456c0b0c6ac3fed84e651ee331
Dominic LoBue <dominic.lobue@gmail.com>
2012-06-15 18:20:06 -0700

    dirty repos and submodules are not allowed

--- cookbooks/chef-fixes/libraries/git_provider.rb
+++ cookbooks/chef-fixes/libraries/git_provider.rb
@@ -1,5 +1,13 @@
 
 class Chef::Provider::Git
+  alias _current_revision_matches_target_revision? current_revision_matches_target_revision?
+
+  def current_revision_matches_target_revision?
+    revmatch = _current_revision_matches_target_revision?
+    return revmatch unless revmatch
+    command = "git diff --no-ext-diff --quiet --exit-code HEAD"
+    shell_out!(command, :cwd => @new_resource.destination, :returns => [0,1]).exitstatus == 0
+  end
 
   def checkout
     sha_ref = target_revision
@@ -11,7 +19,8 @@ class Chef::Provider::Git
   def enable_submodules
     if @new_resource.enable_submodules
       Chef::Log.info "#{@new_resource} enabling git submodules"
-      command = "git submodule update --init --recursive"
+      command = 'git submodule foreach --recursive "git reset --hard; git fetch; git fetch --tags"'
+      command << " && git submodule update --init --recursive"
       shell_out!(command, run_options(:cwd => @new_resource.destination, :command_log_level => :info))
     end
   end
