22e46a9e9cddff0b7519f177e9c3dec19e39ef67
Dominic LoBue <dominic.lobue@gmail.com>
2012-07-09 19:41:16 -0700

    /bin/env doesn't exist, and forgot to get the last argument from the command line

--- cookbooks/solo_client/files/default/delete_sdb_record.py
+++ cookbooks/solo_client/files/default/delete_sdb_record.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 import sys
 import socket
@@ -59,5 +59,5 @@ def main(domain, key, deployment):
             sys.exit(1)
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
 
