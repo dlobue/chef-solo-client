0f458fd2a4664f057ada7d937614da809a57667e
Dominic LoBue <dominic.lobue@gmail.com>
2012-07-25 15:47:44 -0700

    need to dereference annotated tags otherwise chef will keep checking out the same tag over and over again.

--- cookbooks/chef-fixes/libraries/git_provider.rb
+++ cookbooks/chef-fixes/libraries/git_provider.rb
@@ -1,9 +1,16 @@
 
 class Chef::Provider::Git
-  alias _current_revision_matches_target_revision? current_revision_matches_target_revision?
+  def dereferenced_target_revision
+    targetrev = target_revision
+    # a 128 means not found
+    result = shell_out!("git rev-parse #{targetrev}^{commit}", :cwd => cwd, :returns => [0,128]).stdout.strip
+    sha_hash?(result) ? result : targetrev
+  end
 
   def current_revision_matches_target_revision?
-    revmatch = _current_revision_matches_target_revision?
+    revmatch = ((!@current_resource.revision.nil?) &&
+                (dereferenced_target_revision.strip.to_i(16) == @current_resource.revision.strip.to_i(16))
+               )
     return revmatch unless revmatch
     command = "git diff --no-ext-diff --quiet --exit-code HEAD"
     shell_out!(command, :cwd => @new_resource.destination, :returns => [0,1]).exitstatus == 0
