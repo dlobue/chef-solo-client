40620281016b157fbabf605228883def055429f6
Dominic LoBue <dominic.lobue@gmail.com>
2012-07-26 12:51:34 -0700

    refactor helper library out of misc and into the chef-helpers cookbook to make them easier to find

---
+++ cookbooks/chef-helpers/libraries/promise.rb
@@ -0,0 +1,25 @@
+# Computes the expression it was initialized with only when it is needed.
+# acts like the resulting value of that expression, and should pretend to be of
+# the resulting value type.
+# used like so:
+# foo = 'one'
+# bar = 'two'
+# s = Promise.new { foo + bar }
+# s.to_s # results in the string 'onetwo'
+# foo = 'three'
+# bar = 'four'
+# s.to_s # results in the string 'threefour'
+
+class Promise < Proc
+    undef_method :instance_of?
+    undef_method :is_a?
+    undef_method :kind_of?
+    undef_method :respond_to?
+    undef_method :class
+    undef_method :to_s
+    undef_method :display
+    def method_missing(method, *args, &block)
+        call.send(method, *args, &block)
+    end
+end
+
--- cookbooks/chef-helpers/libraries/str_to_bytes.rb
+++ cookbooks/chef-helpers/libraries/str_to_bytes.rb
@@ -1,17 +1,4 @@
-
-class Promise < Proc
-    undef_method :instance_of?
-    undef_method :is_a?
-    undef_method :kind_of?
-    undef_method :respond_to?
-    undef_method :class
-    undef_method :to_s
-    undef_method :display
-    def method_missing(method, *args, &block)
-        call.send(method, *args, &block)
-    end
-end
-
+# helper function to convert a human-readable string to an integer
 
 def str_to_bytes(s)
     r = s.match(/^[0-9]+/)
