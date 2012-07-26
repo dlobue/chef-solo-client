add191059b919a2ad32be941770c3acd7fae484e
Dominic LoBue <dominic.lobue@gmail.com>
2012-07-26 14:22:25 -0700

    run the misc::mounts recipe in dev environments

--- roles/_begin.rb
+++ roles/_begin.rb
@@ -6,6 +6,7 @@ run_list(
     "recipe[apt::apt_fix]",
     "recipe[apt::default]",
     "recipe[upstart::fix_logging]",
+    "recipe[misc::mounts]",
     "recipe[solo_client::default]",
     "recipe[solo_client::begin_end_hub]"
 )
