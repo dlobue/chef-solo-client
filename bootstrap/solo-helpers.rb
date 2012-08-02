83956e3cdaf5957537f3002546264783f19a87ce
Dominic LoBue <dominic.lobue@gmail.com>
2012-08-02 14:08:26 -0700

    comment bootstrap file

--- bootstrap/solo-helpers.rb
+++ bootstrap/solo-helpers.rb
@@ -1,7 +1,23 @@
+# this file serves several purposes. the first is that it adds the remainder of
+# the chef configuration, namely the hooks that pull cookbooks from git, and
+# add the lockr recipes to the runlist. I do not put these in /etc/chef/solo.rb
+# because it is outside of git and not easily updated or tracked.
+#
+# it also adds features to chef that are necessary for those hooks to work.
+# Putting them in a cookbook library would be too late.
+#
+# lastly it fixes problems in chef that, like the hooks, would be loaded too
+# late were they put in a cookbook library.
+#
+# this needs to be refactored badly
+
+
 cookbooks_path = Array(Chef::Config[:cookbook_path]).detect{|e| e =~ /\/cookbooks\/*$/ }
 Ohai::Config[:plugin_path] << File.expand_path(File.join(cookbooks_path, '..', 'ohai_plugins'))
 
-#add notifications that are run before the run begins
+#add a hook entry point that is executed before the chef run begins
+#the primary purpose of this is to checkout the latest version of the cookbooks
+#from git.
 class Chef::Client
   def self.clear_notifications
     @pre_run_notifications = nil
@@ -24,6 +40,10 @@ class Chef::Client
     end
   end
   if public_method_defined? :setup_run_context
+    #sometime between chef 0.9.18 and 0.10.4 chef's was refactored in a way
+    #that broke the hook I used to always ensure that the global lock was
+    #always grabbed before any recipes were executed. this is to add the hook
+    #back in.
     alias _build_node build_node
     def build_node
       _build_node
@@ -36,10 +56,14 @@ class Chef::Client
   end
 end
 
+#another part of my fix for the chef refactoring that broke my lockr insertion
+#point
 class Chef::RunStatus
   attr_accessor :client
 end
 
+
+#RunList emulates almost all other array methods except insert. I required an insert in order to always ensure the global lock was acquired by removing the possiblity of human error.
 class Chef::RunList
   def insert(idx, run_list_item)
     run_list_item = run_list_item.kind_of?(RunListItem) ? run_list_item : parse_entry(run_list_item)
@@ -53,6 +77,10 @@ end
 class Chef::Node
 
   # Consume data from ohai and Attributes provided as JSON on the command line.
+  # ohai data takes precedence over data provided in JSON file, but data in
+  # JSON file should take precendence over cookbooks. I had always assumed this
+  # was the way that chef worked. Turns out it didn't, so this fix is to make
+  # chef conform to my expectations.
   def consume_external_attrs(ohai_data, json_cli_attrs)
     Chef::Log.debug("Extracting run list from JSON attributes provided on command line")
     consume_attributes(json_cli_attrs)
@@ -70,6 +98,9 @@ end
 
 
 
+# pull down the latest cookbooks and recipes from git before every run.
+# because of the placement of the 'before_starting_run' hook the git pull
+# occurs before any of the recipes are loaded.
 Chef::Client.before_starting_run do |json_attribs|
     git_branch = Chef::Config[:git_branch]
     git_repo = Chef::Config[:git_repo]
@@ -126,6 +157,10 @@ Chef::Client.before_starting_run do |json_attribs|
     end
 end
 
+# to rule out the possibility of human error (forgetting to add a the
+# lockr_acquire recipe to the runlist, or adding it in the wrong place), use a
+# hook to insert the lockr_acquire recipe in the run_list so we can be sure
+# that we'll never have downtime before chef was updating code.
 Chef::Client.when_run_starts do |run_status|
   if Chef::Config[:use_lockr]
     run_status.node.run_list.insert(0, "recipe[solo_client::lockr_acquire]")
@@ -137,7 +172,9 @@ Chef::Client.when_run_completes_successfully do |run_status|
   release_lockr(run_status.node) if Chef::Config[:use_lockr]
 end
 
+
 #monkey-patch the runner loop so that we can send it a SIGALRM and start the next loop early.
+# this also is where the 'before_starting_run' hook is executed.
 class Chef::Application::Solo
 
   def configure_logging
@@ -203,6 +240,10 @@ end
 
 
 #Monkey patch the run_context class to use require instead of kernel.load
+#require only loads a library once. calling it multiple times on the same file
+#does nothing after the first time it is invoked.
+#kernel.load however loads a library every time it is called, which breaks my
+#monkey-patches. so it has to go.
 class Chef
   class RunContext
     private
@@ -215,4 +256,6 @@ class Chef
     end
   end
 end
+
+
 
