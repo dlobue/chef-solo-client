06bcb3fe7a643ea370307556eb5efb85c9d9a583
Dominic LoBue <dominic.lobue@gmail.com>
2012-08-02 14:54:40 -0700

    add documentation for the upstart cookbook and the upstart_event resource.

--- cookbooks/upstart/recipes/fix_logging.rb
+++ cookbooks/upstart/recipes/fix_logging.rb
@@ -1,3 +1,7 @@
+# This recipe is to create the /var/log/upstart directory, which for some
+# reason doesn't exist in ubuntu AMIs, and upstart won't create it on its own.
+# As a result without this directory logs for jobs with 'console log' won't be
+# created.
 
 directory "/var/log/upstart" do
   only_if { node.current_user == 'root' }
