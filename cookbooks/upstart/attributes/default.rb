cb20c0dbd65a776a86fdfcb76280ec221fbf7f7c
Dominic LoBue <dominic.lobue@gmail.com>
2012-10-17 17:42:48 -0700

    add support for user jobs

--- cookbooks/upstart/attributes/default.rb
+++ cookbooks/upstart/attributes/default.rb
@@ -1,5 +1,7 @@
 
 require 'pathname'
 
-default.upstart.conf_dir = Pathname.new '/etc/init'
+default.upstart.system_conf_dir = Pathname.new '/etc/init'
+default.upstart.user_conf_dir = Pathname.new(node.etc.passwd[node.current_user].dir) + '.init'
+default.upstart.conf_dir = current_user == "root" ? upstart.system_conf_dir : upstart.user_conf_dir
 
