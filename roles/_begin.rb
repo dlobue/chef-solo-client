ab1bd87c10a0e68605f5ba99d61ce810087b030f
Dominic LoBue <dominic.lobue@gmail.com>
2012-12-06 16:45:36 -0800

    explicitly include the recommends and s3repo recipes to prevent clashes with 3rd-party apt cookbook

--- roles/_begin.rb
+++ roles/_begin.rb
@@ -4,7 +4,8 @@ description "recipes that should always run first."
 
 run_list(
     "recipe[apt::apt_fix]",
-    "recipe[apt::default]",
+    "recipe[apt::recommends]",
+    "recipe[apt::s3repo]",
     "recipe[upstart::fix_logging]",
     "recipe[misc::mounts]",
     "recipe[solo_client::default]",
